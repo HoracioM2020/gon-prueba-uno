739672e9fb918de754ef3acd93f343be304a516a
Gonzalo Traverso <gonzalo.traverso@kantox.com>
2021-02-25 12:57:46 -0300

    mejorar formato, agregar parentesis faltante, etc

--- impuesto-uca.py
+++ impuesto-uca.py
@@ -1,22 +1,22 @@
-ipi={"Dtomin":556.02, "Indif":85528,"Basemax":14839.02, "%-min": 18, "%-max": 32}
+ipi={"Dtomin": 556.02, "Indif": 85528, "Basemax": 14839.02, "%-min": 18, "%-max": 32}
 print(ipi)
 print(ipi["Basemax"])
 
 def main():
-  a=int(input("su ingreso anual: "))
-  print()
-  b=impuesto(a)
-  print("El impuesto es de "), 
-  c=round(b,0)
-  print(c)
+    a = int(input("su ingreso anual: "))
+    print()
+    b = impuesto(a)
+    print("El impuesto es de ") 
+    c = round(b,0)
+    print(c)
 
 def impuesto(a):
-        if a<=ipi["Indif"]:
-            impuesto=ipi["%-min"]/100 *a-ipi["Dtomin"]
-        if impuesto<0:
-            impuesto=0
-            print("Usted es un afortunado")
-        elif a>ipi["Indif"]:
-            impuesto=ipi["Basemax"]+(a-ipi["Indif"]*ipi["%-max"]/100
+    if a <= ipi["Indif"]:
+        resultado = (ipi["%-min"] / 100) * a - ipi["Dtomin"]
+    if a < 0:
+        resultado = 0
+        print("Usted es un afortunado")
+    elif a > ipi["Indif"]:
+        resultado = ipi["Basemax"] + (a - ipi["Indif"] * ipi["%-max"]) / 100
 
-        return(impuesto)
+    return resultado
